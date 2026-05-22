95e4cc1af5105d8225fb3b0fa067a8f7da7fed55
Manoel Oliveira <manoel7oliv@gmail.com>
2026-05-22 19:39:04 -0300

    Props Booleanas

--- meu-primeiro-react/first-react/src/App-2.jsx
+++ meu-primeiro-react/first-react/src/App-2.jsx
@@ -2,12 +2,12 @@ import Button from "./Componentes/Button/Button";
 import Card from "./Componentes/Card/Card";
 
 function App() {
-    
+
     return (
         <>
-            <Button />  {/*importei o button do arquivo Button.jsx*/}
+            <Button text="Salvar" disable />  {/*importei o button do arquivo Button.jsx*/}
             <Button />
-            <Button />
+            <Button disable />
             <Card titulo="titulo 1" descrição="descrição 1" />
             <Card titulo="titulo 2" descrição="descrição 2" />
             <Card titulo="titulo 3" descrição="descrição 3" />
--- meu-primeiro-react/first-react/src/Componentes/Button/button.jsx
+++ meu-primeiro-react/first-react/src/Componentes/Button/button.jsx
@@ -1,11 +1,12 @@
 import styles from "./Button.module.css"  // Esse é o css module, é uma forma de evitar
 //  vazamento quando colocamos classes com o mesmo nome. Essa é a sintaxe de importação. E a sintaxe pra usar é className={styles.nomedaclassaqui}
 
-function Button() {
+function Button({ text = "Click default", disable }) {
 
     return (
-        <button className={styles.btn}>
-            <span className={styles.title}>Click</span>
+        //<button className={styles.btn} disabled={disable}>
+        <button className={`${styles.btn} ${disable ? styles.disable : ""}`} disabled={disable}>
+            <span className={styles.title}>{text} - {disable} - {typeof disable} </span>
         </button>
     )
 }

352f1142e72da9057cd6afa5bfbb1de6ea80bcae
Manoel Oliveira <manoel7oliv@gmail.com>
2026-05-22 20:48:50 -0300

    Funções como parâmetros

--- meu-primeiro-react/first-react/src/App-2.jsx
+++ meu-primeiro-react/first-react/src/App-2.jsx
@@ -3,10 +3,15 @@ import Card from "./Componentes/Card/Card";
 
 function App() {
 
+    function buttonClicado() {
+        console.log('Button clicado app')
+    }
+
     return (
         <>
             <Button text="Salvar" disable />  {/*importei o button do arquivo Button.jsx*/}
-            <Button text="Salvar 2" />
+            <Button text="Salvar 2" onClique={buttonClicado} />
+            <Button text="Salvar 3" />
             <Button disable />
             <Card titulo="titulo 1" descrição="descrição 1" />
             <Card titulo="titulo 2" descrição="descrição 2" />
--- meu-primeiro-react/first-react/src/Componentes/Button/button.jsx
+++ meu-primeiro-react/first-react/src/Componentes/Button/button.jsx
@@ -1,16 +1,19 @@
 import styles from "./Button.module.css"  // Esse é o css module, é uma forma de evitar
 //  vazamento quando colocamos classes com o mesmo nome. Essa é a sintaxe de importação. E a sintaxe pra usar é className={styles.nomedaclassaqui}
 
-function btnClicado () {
-    console.log('btn')
-}
+/*
+Isso esta incorreto, o certo é o useEffect
+window.addEventListener("resize", () => {console.log('resize')})
+*/
+function Button({ text = "Click default", disable, onClique }) {
+    // A função aqui dentro, pode acessar as props do componente. 
+    function btnClicado() {
+        console.log('btn', text)
 
-
-function Button({ text = "Click default", disable }) {
-
-    function btnClicado () {
-    console.log('btn', text)
-}
+        if (onClique && typeof onClique === 'function') {
+            onClique();
+        }
+    }
 
     return (
         //<button className={styles.btn} disabled={disable}>

8ca975e9f2025baa75d082a0e7d896495f5c0b48
Manoel Oliveira <manoel7oliv@gmail.com>
2026-05-25 00:51:46 -0300

    setInterval + useRef

--- meu-primeiro-react/first-react/src/main.jsx
+++ meu-primeiro-react/first-react/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import App from './App-6-useRef-Dom.jsx'
+import App from './App-7-useRef-Dom-video.jsx'
 
 createRoot(document.getElementById('root')).render(
  // <StrictMode>

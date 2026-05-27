2cfec80079708a36cac5874e23c0842420c3238e
Manoel Oliveira <manoel7oliv@gmail.com>
2026-05-27 00:12:53 -0300

    useId

--- meu-segundo-react/second-react/src/components/InputLabel/InputLabel.jsx
+++ meu-segundo-react/second-react/src/components/InputLabel/InputLabel.jsx
@@ -1,12 +1,16 @@
 import styles from "./InputLabel.module.css"
+import { useId } from "react"
 
 export function InputLabel({ disclaimer, label, value = '0', type = 'text', onValueChange }) {
+
+    const idSingle = useId();
+
     return (
 
         <>
             <div className={styles.inputGroup}>
-                <label>Celsius:</label>
-                <input type={type} value={value} onChange={(e) => {
+                <label htmlFor={idSingle}>Celsius:</label>
+                <input id={idSingle} type={type} value={value} onChange={(e) => {
                     onValueChange(e.target.value)
                 }} />
             </div>

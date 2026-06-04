1988103d3c76f1dfab5baa36a077de7529b1e3f3
Manoel Oliveira <manoel7oliv@gmail.com>
2026-06-04 18:37:50 -0300

    Activity e useLayoutEffect

---
+++ meu-segundo-react/second-react/src/App-8.jsx
@@ -0,0 +1,57 @@
+import { Activity, useLayoutEffect, useState, useRef } from "react"
+
+function MeuComponent() {
+
+  const [count, setCount] = useState(0);
+  const videoRef = useRef(null);
+
+  useLayoutEffect(() => {
+    console.log('MeuComponent entra na tela ');
+    console.log(videoRef.current);
+    videoRef?.current?.play();
+    return () => {
+      console.log('MeuComponent sai da tela ');
+      console.log(videoRef.current);
+      videoRef?.current?.pause();
+    }
+  }, [])
+
+  return (
+    <>
+      <p>Count: {count} </p>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+
+      <video width="300px" ref={videoRef} controls>
+
+        <source src="https://b471b51d-3e69-4dbb-b2e6-bc5c13340f7b.mdnplay.dev/shared-assets/videos/flower.webm" type="video/mp4" />
+
+      </video>
+
+    </>
+  )
+}
+
+
+function App() {
+
+  const [show, setShow] = useState(true);
+
+  return (
+
+    <div>
+
+      <button onClick={() => setShow(!show)}>{show ? 'Esconder' : 'Mostrar'}</button>
+      {/* {show && <MeuComponent />} Renderização condicional */}
+      {/* <div style={{ display: show ? "block" : "none" }}>
+        <MeuComponent />
+      </div> */}
+
+      <Activity mode={show ? 'visible' : 'hidden'}>
+        <MeuComponent />
+      </Activity>
+
+    </div>
+  )
+}
+
+export default App
--- meu-segundo-react/second-react/src/main.jsx
+++ meu-segundo-react/second-react/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 //import './index.css'
-import App from './App-7.jsx'
+import App from './App-8.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+   <StrictMode>
     <App />
-  </StrictMode>,
+   </StrictMode>,
 )

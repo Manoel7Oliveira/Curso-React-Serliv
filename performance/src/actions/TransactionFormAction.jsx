b229732f7310cffa4849bda3d77cbc0f5155d67f
Manoel Oliveira <manoel7oliv@gmail.com>
2026-06-30 00:34:22 -0300

    Atualização no TransactionFormAction, simulando as verificações no lado do cliente e no lado do servidor

--- performance/src/actions/TransactionFormAction.jsx
+++ performance/src/actions/TransactionFormAction.jsx
@@ -8,39 +8,39 @@ export const createTransactionAction = async (previousState, formData) => {
 
     if (!client) {
         return {
-            sucess: false, error: 'O cliente é obrigatorio'
+            success: false, error: 'O cliente é obrigatorio'
         }
     }
 
     if (client.length < 3) {
         return {
-            sucess: false, error: 'O cliente precisa ter no minimo tres caracteres'
-        }
-    }
-
-    if (!amount) {
-        return {
-            sucess: false, error: 'A quantidade é obrigatoria'
-        }
-    }
-
-    if (parseFloat(amount) <= 0) {
-        return {
-            sucess: false, error: 'A quantidade precisar ser maior que zero'
+            success: false, error: 'O cliente precisa ter no minimo três caracteres'
         }
     }
 
     await new Promise(resolve => setTimeout(resolve, 2000));
 
+    if (!amount) {
+        return {
+            success: false, error: 'A quantidade é obrigatoria'
+        }
+    }
+
+    if (parseFloat(amount) <= 0) {
+        return {
+            success: false, error: 'A quantidade precisa ser maior que zero'
+        }
+    }
+
     return {
 
-        sucess: true,
+        success: true,
         error: null,
         newData: {
             id: Date.now(),
             client,
             amount: parseFloat(amount),
-            date: dataAleatoria(),
+            date: new Date().toLocaleDateString(),
             status
         }
     }
